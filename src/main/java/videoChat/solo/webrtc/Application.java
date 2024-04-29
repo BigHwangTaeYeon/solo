1fcd3f03b6fa1e7536dd21839aad07caff51fb69
BigHwangTaeYeon <gxd0214@gmail.com>
2024-04-29 21:37:39 +0900

    기본 구조 재구성

---
+++ src/main/java/videoChat/solo/webrtc/Application.java
@@ -0,0 +1,15 @@
+package videoChat.solo.webrtc;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+
+@SpringBootApplication
+@EnableWebSocket
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
--- src/test/java/videoChat/solo/SoloApplicationTests.java
+++ src/test/java/videoChat/solo/SoloApplicationTests.java
@@ -1,13 +1,31 @@
 package videoChat.solo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import videoChat.solo.webrtc.config.WebSocketConfig;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SoloApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private ApplicationContext applicationContext;
 
+	@Test
+	public void contextLoads() throws Exception {
+		if (applicationContext != null) {
+			String[] beans = applicationContext.getBeanDefinitionNames();
+
+			for (String bean : beans) {
+				System.out.println("bean : " + bean);
+			}
+		}
+
+		System.out.println("websocket bean : " + applicationContext.getBean(WebSocketConfig.class).signalHandler());
+	}
 }
